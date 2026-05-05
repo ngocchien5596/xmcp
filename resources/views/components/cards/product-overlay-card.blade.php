0ecc453ae6f96458c73ce2db37111630cbadd443
ngocchien5596 <buingocchien5596@gmail.com>
2026-05-05 21:44:48 +0700

    Adjust product card dimensions to be more compact

--- resources/views/components/cards/product-overlay-card.blade.php
+++ resources/views/components/cards/product-overlay-card.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('products.show', $product['slug']) }}" 
-    class="motion-border-card group relative block h-[420px] w-[260px] overflow-hidden bg-[#222] md:h-[470px] md:w-[340px] lg:w-[360px] reveal reveal-up" 
+    class="motion-border-card group relative block h-[380px] w-[260px] overflow-hidden bg-[#222] md:h-[420px] md:w-[300px] lg:w-[320px] reveal reveal-up" 
     style="overflow: hidden !important;"
     data-animate>
     
