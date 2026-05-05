ee98444578da30c85f7227c90b3f99eac8259308
ngocchien5596 <buingocchien5596@gmail.com>
2026-05-05 21:10:39 +0700

    Update partner logos and reorder Viettel Family before SInvoice

--- config/partners.php
+++ config/partners.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    ['id' => 'p1', 'name' => 'Cổng Dịch vụ công Quốc gia', 'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Logo_C%E1%BB%95ng_D%E1%BB%8Bch_v%E1%BB%A5_c%C3%B4ng_Qu%E1%BB%91c_gia.png/640px-Logo_C%E1%BB%95ng_D%E1%BB%8Bch_v%E1%BB%A5_c%C3%B4ng_Qu%E1%BB%91c_gia.png', 'href' => null],
-    ['id' => 'p2', 'name' => 'Cổng thông tin điện tử Bộ Quốc phòng', 'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Emblem_of_Vietnam.svg/512px-Emblem_of_Vietnam.svg.png', 'href' => null],
-    ['id' => 'p3', 'name' => 'Viettel', 'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Viettel_logo_2021.svg/2560px-Viettel_logo_2021.svg.png', 'href' => null],
-    ['id' => 'p4', 'name' => 'SInvoice', 'image' => 'https://sinvoice.viettel.vn/Content/images/logo-sinvoice.png', 'href' => null],
-    ['id' => 'p5', 'name' => 'Viettel Family', 'image' => 'https://viettelfamily.com/assets/img/logo.png', 'href' => null],
+    ['id' => 'p1', 'name' => 'Cổng Dịch vụ công Quốc gia', 'image' => '/images/dv.webp', 'href' => 'https://dichvucong.gov.vn/'],
+    ['id' => 'p2', 'name' => 'Cổng thông tin điện tử Bộ Quốc phòng', 'image' => '/images/bqp.webp', 'href' => 'https://mod.gov.vn/'],
+    ['id' => 'p3', 'name' => 'Viettel', 'image' => '/images/vt.webp', 'href' => 'https://viettel.com.vn/'],
+    ['id' => 'p5', 'name' => 'Viettel Family', 'image' => '/images/logo vtfml.webp', 'href' => 'https://viettelfamily.com/'],
+    ['id' => 'p4', 'name' => 'SInvoice', 'image' => '/images/si.webp', 'href' => 'https://sinvoice.viettel.vn/'],
 ];
--- resources/views/sections/common/partner-section.blade.php
+++ resources/views/sections/common/partner-section.blade.php
@@ -1,6 +1,6 @@
 <section class="bg-[var(--color-surface-gray)] py-12 md:py-16">
     <div class="mx-auto max-w-[1440px] px-4 md:px-8">
-        @include('components.ui.section-title', ['title' => 'Khách hàng - Đối tác'])
+        @include('components.ui.section-title', ['title' => 'Liên kết'])
         <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-5" data-stagger-container>
             @foreach($partners as $partner)
                 @include('components.cards.partner-logo-card', ['partner' => $partner])
